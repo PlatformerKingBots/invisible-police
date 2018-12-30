e0333a736c1bfc544b937237cfa4c32afd0612e7
PlatformerKingBots <45886615+PlatformerKingBots@users.noreply.github.com>
2018-12-29 22:19:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var toasters = [
+  'https://media.giphy.com/media/71loenMVZGgN2/giphy.gif',
+  'https://media.giphy.com/media/VWVthRb92CS8E/giphy.gif',
+  'https://media.giphy.com/media/71loenMVZGgN2/giphy.gif',
+];
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-
+  
 });
 
 client.login('NTI4NzcxMDA5MTUxNjMxMzYw.DwnIYA.dET4n2yb-hCcP_jOQMRw3c722pU');
