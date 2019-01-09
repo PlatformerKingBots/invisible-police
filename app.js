012435163d55252530a3352dbb0019acd34dcd61
PlatformerKingBots <45886615+PlatformerKingBots@users.noreply.github.com>
2019-01-09 17:48:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
+client.on('typingStart', (channel, user) => {
+  channel.send({embed:{
+    author: {
+      name: user.tag,
+      avatar_url: user.avatarURL
+    },
+    description: ':rage: '+ user.tag+ ' is typing while offline!!! :rage:
 });
 
 client.login(process.env.token);
